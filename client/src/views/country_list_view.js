13313b7d82616d21251bf48f5661a7ff0e444bde
Douglas Crooke <douglas.crooke+codeclan@googlemail.com>
2017-04-19 14:43:10 +0100

    add event listener to each checkbox for use with database operations

--- client/src/views/country_list_view.js
+++ client/src/views/country_list_view.js
@@ -7,8 +7,21 @@ CountryList.prototype = {
     var ul = document.querySelector('#countries-list')
     countries.forEach(function (country) {
       var li = document.createElement('li')
-      li.innerText = country.name
+      var label = document.createElement('label')
+      var checkbox = document.createElement('input')
+      checkbox.type = 'checkbox'
+      checkbox.id = country.name
+      checkbox.value = country.name
+      label.for = country.name
+      label.innerHTML = country.name
+      ul.listStyle = 'none'
       ul.appendChild(li)
+      li.appendChild(checkbox)
+      li.appendChild(label)
+      checkbox.addEventListener('change', function () {
+        console.log('changed ', checkbox.value)
+        console.log('checked? ', checkbox.checked)
+      })
     })
   }
 }
